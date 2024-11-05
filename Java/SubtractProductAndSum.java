98a9b162ca442602bb2761db5a5cc21c4289c49e
cosmos963 <yashprajapati264@gmail.com>
2024-11-05 19:15:15 +0530

    1281. Subtract the Product and Sum of Digits of an Integer

--- Java/SubtractProductAndSum.java
+++ Java/SubtractProductAndSum.java
@@ -1,7 +1,7 @@
-package leetcode.Java;
+package Java;
 
 // 1281. Subtract the Product and Sum of Digits of an Integer
-// https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+// https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/description/
 
 // Time : O(d) , where d = number of digits in the integer n
 // Space : O(1)
