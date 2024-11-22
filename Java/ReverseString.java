9504727ecd7b985d07fc47fd2eafd792bbfb5f69
cosmos963 <yashprajapati264@gmail.com>
2024-11-23 00:49:09 +0530

    344. Reverse String

---
+++ Java/ReverseString.java
@@ -0,0 +1,20 @@
+// 344. Reverse String
+// https://leetcode.com/problems/reverse-string/description/
+
+// Time complexity : O(n), where n is the length of the array
+// Space complexity : O(1), No additional data structures or auxiliary space are used.
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+
+        while(start < end){
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
