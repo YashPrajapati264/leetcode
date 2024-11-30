5d0f44b6febb49251eb3978785a4fa56c8973527
cosmos963 <yashprajapati264@gmail.com>
2024-11-30 23:49:25 +0530

    189. Rotate Array

--- Java/RotateArrayToRightKSteps.java
+++ Java/RotateArrayToRightKSteps.java
@@ -1,7 +1,9 @@
 // 189. Rotate Array
 // https://leetcode.com/problems/rotate-array/description/
 
-// Time complexity : O(n)
+// Approach : Reverse-Based Array Rotation, which involves reversing the array in three steps to achieve the desired rotation to the right by k steps.
+
+// Time Complexity : O(n), where n is the number of elements in the array.
 // The method performs three separate reverse operations, each of which takes O(n) time in the worst case. Since these operations are sequential, the overall time complexity remains O(n).
 
 // Space complexity : O(1)
